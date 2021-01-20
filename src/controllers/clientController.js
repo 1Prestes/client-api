5cdcfc28bcccf8384ee59b58d3a7783d9aa4d714
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-20 20:40:49 -0300

    feat: add find one

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -6,9 +6,23 @@ module.exports = {
       const client = await Client.create(req.body);
       return res.send(client);
     } catch (err) {
-      return res
-        .status(400)
-        .send({ error: err });
+      return res.status(400).send({ error: err });
     }
-  }
+  },
+  async index(req, res) {
+    try {
+      const clients = await Client.find({});
+      return res.send(clients);
+    } catch (err) {
+      return res.status(400).send({ error: err });
+    }
+  },
+  async show(req, res) {
+    try {
+      const client = await Client.findOne({ _id: req.params.id });
+      return res.send(client);
+    } catch (err) {
+      return res.status(400).send({ error: err });
+    }
+  },
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const Client = require('../controllers/clientController')
-const AuthController = require('../controllers/authController')
+const Client = require("../controllers/clientController");
+const AuthController = require("../controllers/authController");
 
 router.get("/", (req, res) => {
   res.send("Home");
@@ -9,6 +9,8 @@ router.get("/", (req, res) => {
 
 router.post("/login", AuthController.create);
 
+router.get("/clients", AuthController.isAuth, Client.index);
+router.get("/client/:id", AuthController.isAuth, Client.show);
 router.post("/client", AuthController.isAuth, Client.create);
 
 module.exports = router;
