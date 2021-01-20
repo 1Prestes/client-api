47314b7f7248a4cbf7f75b21ba8b5c05aaac3990
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-20 15:17:18 -0300

    feat: create login with JWT

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,34 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
-router.get('/', (req, res) => {
-    res.send('Home')
-})
+const SECRET = "zukk";
 
-router.post('/login', (req, res) => {
-    const {login, password} = req.body
-    
-    if(login === 'admin' && password === 1234) res.send('You are logged!')
-    else res.status(401).send('Invalid login or password!')
-})
+function isAuth(req, res, next) {
+  const token = req.headers["x-access-token"];
+
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) return res.status(401).end();
+
+    req.userId = decoded.userId;
+    next();
+  });
+}
+
+router.get("/", (req, res) => {
+  res.send("Home");
+});
+
+router.post("/login", (req, res) => {
+  const { login, password } = req.body;
+
+  if (login === "admin" && password === 1234) {
+    const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: '30m' });
+    res.json({ auth: "You are logged!", token });
+  } else res.status(401).send("Invalid login or password!");
+});
+
+router.get("/clients", isAuth, (req, res) => {
+  res.json({ name: "Neumir", UF: "SP" });
+});
 
 module.exports = router;
