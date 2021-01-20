22b7ff21dbab9e183d0a9f65ecec67f58d8a4af1
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-20 19:54:28 -0300

    refactor: update schema type

--- src/models/client.js
+++ src/models/client.js
@@ -2,15 +2,13 @@ const mongoose = require("../config/database");
 const { Schema } = mongoose;
 
 const clientSchema = new Schema({
-  name: String,
-  address: String,
-  neighborhood: String,
-  city: String,
-  uf: String,
-  telephone: String,
-  email: String,
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  neighborhood: { type: String, required: true },
+  city: { type: String, required: true },
+  uf: { type: String, required: true },
+  telephone: { type: String, unique: true },
+  email: { type: String, unique: true, required: true },
 });
 
-const client = mongoose.model("Client", clientSchema);
-
-module.exports = client;
+module.exports = mongoose.model("Client", clientSchema);
