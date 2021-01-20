93d5ab1a7ab80cca5fb6abcc03f9fa7f0df86ae2
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-20 19:49:54 -0300

    refactor: responsibilities separated of Routes with authController

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,39 +1,14 @@
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
 
 const Client = require('../controllers/clientController')
-
-function isAuth(req, res, next) {
-  const token = req.headers["x-access-token"];
-  if (!token)
-    return res.status(401).json({ auth: false, message: "No token provided." });
-
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err)
-      return res
-        .status(500)
-        .json({ auth: false, message: "Failed to authenticate token." });
-
-    req.userId = decoded.userId;
-    next();
-  });
-}
+const AuthController = require('../controllers/authController')
 
 router.get("/", (req, res) => {
   res.send("Home");
 });
 
-router.post("/login", (req, res) => {
-  const { login, password } = req.body;
+router.post("/login", AuthController.create);
 
-  if (login === "admin" && password === 1234) {
-    const token = jwt.sign({ userId: 1 }, process.env.SECRET, {
-      expiresIn: "30m",
-    });
-    res.json({ auth: "You are logged!", token });
-  } else res.status(401).send("Invalid login or password!");
-});
-
-router.post("/client", isAuth, Client.create);
+router.post("/client", AuthController.isAuth, Client.create);
 
 module.exports = router;
